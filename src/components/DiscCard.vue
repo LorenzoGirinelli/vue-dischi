d5c4b086b395280da21c6c0ba88e8e8a9852152b
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-12-22 16:34:49 +0100

    main dischi

---
+++ src/components/DiscCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card contaier">
+        <div>
+            <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+        </div>
+
+        <div class="disc-name">
+            <h2>Nome</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card'
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    width: calc(100% / 5);
+    background-color: #2e3a46;
+    &-image{
+        img {
+        }
+    }
+
+    &-name {
+        text-align: center;
+    }
+}
+</style>
--- src/components/DiscList.vue
+++ src/components/DiscList.vue
@@ -1,12 +1,31 @@
 <template>
     <section>
-        hi
+        <div class="container">
+            <DiscCard />
+        </div>
     </section>
 </template>
 
 <script>
+import axios from 'axios';
+import DiscCard from './DiscCard';
+
 export default {
-    nome: 'DiscList'
+    nome: 'DiscList',
+    components: {
+        DiscCard
+    },
+    data: function () {
+        return {
+            disc: [],
+        }
+    },
+    created: function() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((response) => {
+            this.disc = response.data;
+        });
+    }
 }
 </script>
 
